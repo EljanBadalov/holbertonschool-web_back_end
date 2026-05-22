4e9762cde32451c971d7a74275ea074469100c62
EljanBadalov <elcanbadalov06@gmail.com>
2026-05-22 16:29:29 +0400

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ from typing import Tuple, Union
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns the expected result."""
     
-    return (k, v ** 2) 
+    return (k, v ** 2)
