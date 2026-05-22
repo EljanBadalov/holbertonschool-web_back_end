0ffad9ab533572eb98ac15af82c539eb25513f60
EljanBadalov <elcanbadalov06@gmail.com>
2026-05-22 15:33:15 +0400

    task 2

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Module for concat function."""
+"""Module documentation."""
+
 
 def concat(str1: str, str2: str) -> str:
+    """Returns the expected result."""
     return str1 + str2
