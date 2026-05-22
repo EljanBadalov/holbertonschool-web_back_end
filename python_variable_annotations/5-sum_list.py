bf99235c86ffbac67b5448cc226d8b6730854835
EljanBadalov <elcanbadalov06@gmail.com>
2026-05-22 15:54:02 +0400

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module documentation."""
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns the expected result."""
 
     return sum(input_list)
