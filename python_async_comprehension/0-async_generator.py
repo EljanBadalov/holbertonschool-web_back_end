8f4b792f3c8fea03242640b60ef3eb92c507d5c7
EljanBadalov <elcanbadalov06@gmail.com>
2026-05-23 19:27:24 +0400

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module documentation."""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Returns the expected result."""
+
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
