cb2f61f177cffe3b3f8c42bc11dfaa83ac879816
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-06-11 00:31:37 +0400

    Update 0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""Pagination helper function module."""
 
-def index_range(page: int, page_size: int):
-	start_index = (page - 1) * page_size
-	end_index = page * page_size
-	return (start_index, end_index)
 
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple containing start and end indexes."""
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
+	
