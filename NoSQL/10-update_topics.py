dad68e62bc1e8de9fcc2dd54d51e5c17f2c2d721
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-06-20 17:07:45 +0400

    Create 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for updating school topics."""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Change all topics of a school document."""
+    
+    mongo_collection.updateMany({name: name}, {$set: {topics: topics})
