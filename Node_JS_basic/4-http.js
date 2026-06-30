cd7832b4d8c9426e304a9e1369c66c4fe4bd2283
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-06-30 18:31:26 +0400

    Update 4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,9 @@
-import http from 'http'
+const http = require('http')
 
 const app = http.createServer((req, res) => {
     res.end("Hello Holberton School!")
 })
 
 app.listen(1245)
+
+module.exports = app
