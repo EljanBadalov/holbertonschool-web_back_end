19bf2a63da638d8eb91caeb76cce15435128e837
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-06-30 19:07:31 +0400

    Update 5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,21 +1,26 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async.js');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    
     const databasePath = process.argv[2];
 
     countStudents(databasePath)
       .then((studentsInfo) => {
-        res.end(`This is the list of our students\n${studentsInfo}`);
+        res.end(studentsInfo);
       })
       .catch((err) => {
-        res.end(`This is the list of our students\n${err.message}`);
+        res.end(err.message);
       });
+  } else {
+    res.end();
   }
 });
 
