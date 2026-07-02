c138691d5fc1e8f13cfe66b28a664d828a43b838
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-07-02 18:58:39 +0400

    Add files via upload

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,41 @@
+import readDatabase from "../utils";
+
+export default class StudentsController {
+  static async getAllStudents(request, response) {
+    try {
+      const databasePath = process.argv[2];
+      const data = await readDatabase(databasePath);
+
+      const fields = Object.keys(data).sort();
+
+      let output = `This is the list of our students`;
+
+      for (const field of fields) {
+        const firstnames = data[field];
+
+        const lineMsg = `Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(", ")}`;
+        output += `\n${lineMsg}`;
+      }
+
+      response.status(200).send(output);
+    } catch (error) {
+      response.status(500).send("Cannot load the database");
+    }
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    const major = request.params.major;
+    if (major !== "CS" && major !== "SWE") {
+      return response.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    try {
+      const databasePath = process.argv[2];
+      const data = await readDatabase(databasePath);
+      const majorList = data[major];
+      return response.status(200).send(`List: ${majorList.join(', ')}`)
+    } catch {
+      response.status(500).send('Cannot load the database');
+    }
+  }
+}
