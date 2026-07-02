e33f70481eed4902cd21cc8c9b9f1ff31ff4a37a
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-07-02 19:04:41 +0400

    Update StudentsController.js

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,4 +1,4 @@
-import readDatabase from "../utils";
+import readDatabase from '../utils';
 
 export default class StudentsController {
   static async getAllStudents(request, response) {
@@ -13,19 +13,19 @@ export default class StudentsController {
       for (const field of fields) {
         const firstnames = data[field];
 
-        const lineMsg = `Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(", ")}`;
+        const lineMsg = `Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`;
         output += `\n${lineMsg}`;
       }
 
       response.status(200).send(output);
     } catch (error) {
-      response.status(500).send("Cannot load the database");
+      response.status(500).send('Cannot load the database');
     }
   }
 
   static async getAllStudentsByMajor(request, response) {
     const major = request.params.major;
-    if (major !== "CS" && major !== "SWE") {
+    if (major !== 'CS' && major !== "SWE') {
       return response.status(500).send('Major parameter must be CS or SWE');
     }
 
